85b4c2a04c68e43ca6ed9ed6d1355c925d7bf61e
xuqidong <xuqidong@xuqidong.local>
2017-05-22 16:46:56 +0800

    微信好友数据统计和分析
    增加好友地图分布，引入flask

--- city.py
+++ city.py
@@ -2,16 +2,17 @@
 from echarts import Echart, Legend, Series, Axis, Bar
 from collections import Counter
 import jieba.analyse
+import requests
 
 
 def totalCity(friends):
     fileName = 'totalCity.txt'
     with open(fileName, 'w') as fp:
         for i in friends:
-            Province = i["Province"].encode('utf8')
+            City = i["Province"].encode('utf8')
             # City = i["City"].encode('utf8')
-            if len(Province) != 0:
-                fp.write('%s \n' % Province)
+            if len(City) != 0:
+                fp.write('%s \n' % City)
 
     # 统计
     bill_path = r'totalCity.txt'
@@ -29,8 +30,39 @@ def totalCity(friends):
                 fw.write("%s,%d\n" % (k.encode('utf-8'), v))
 
     # 用echart画图
-    chart = Echart(u'%s的微信好友省份分布统计' % (friends[0]['NickName']), 'from WeChat by xuqidong')
-    chart.use(Bar('China', valueList))
-    chart.use(Legend(['GDP']))
-    chart.use(Axis('category', 'left', data=nameList))
-    chart.plot()
+    # chart = Echart(u'%s的微信好友省份分布统计' % (friends[0]['NickName']), 'from WeChat by xuqidong')
+    # chart.use(Bar('China', valueList))
+    # chart.use(Legend(['GDP']))
+    # chart.use(Axis('category', 'left', data=nameList))
+    # chart.plot()
+
+    # {name: '海门', value: 9},
+    dataList = []
+
+    i = 0
+    for name in nameList:
+        value = valueList[i]
+        name = name
+        i += 1
+        if check_contain_chinese(name.encode('utf-8')):
+            GPS = geocodeG(name)
+            if GPS:
+                dataList.append({'name': name, 'count': value, 'address': GPS})
+    return dataList
+
+def check_contain_chinese(check_str):
+      for ch in check_str.decode('utf-8'):
+          if u'\u4e00' <= ch <= u'\u9fff':
+             return True
+      return False
+
+#使用高德API,
+def geocodeG(address):
+    par = {'address': address, 'key': 'cb649a25c1f81c1451adbeca73623251'}
+    base = 'http://restapi.amap.com/v3/geocode/geo'
+    response = requests.get(base, par)
+    answer = response.json()
+    if len(answer['geocodes'])!=0:
+        GPS=answer['geocodes'][0]['location'].split(",")
+        print address,[float(GPS[0]),float(GPS[1])]
+        return [float(GPS[0]),float(GPS[1])]
--- weChat.py
+++ weChat.py
@@ -3,14 +3,28 @@ import login
 import sex
 import city
 import sign
+import jinja2
+from flask import render_template
+from flask import Flask
+app = Flask(__name__,static_url_path='')
 
-class Start(object):
-    def __init__(self):
-        friends = login.Login().loginWeChat()
-        sex.totalSex(friends)
-        city.totalCity(friends)
-        sign.totalSign(friends)
+@app.route('/')
+def earth_point2():
+    friends = login.Login().loginWeChat()
+    sex.totalSex(friends)
+    cityList = city.totalCity(friends)
+    sign.totalSign(friends)
+    return render_template('earth_point3.html', cityList=cityList)
+
+# class Start(object):
+#     def __init__(self):
+#         friends = login.Login().loginWeChat()
+#         sex.totalSex(friends)
+#         cityList = city.totalCity(friends)
+#         earth_point2(cityList)
+#         sign.totalSign(friends)
 
 
 if __name__ == '__main__':
-    Start()
+    # Start()
+    app.run(debug=True)
